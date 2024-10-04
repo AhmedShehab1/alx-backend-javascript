544a61d8a8d891c38e1f8157014e5fbb2a04fd5e
AhmedShehab1 <legendbtw2003@gmail.com>
2024-10-04 04:44:17 -0700

    Disabled Linting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  task = false;
+  task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = false;
+    var task2 = true;
   }
 
   return [task, task2];
