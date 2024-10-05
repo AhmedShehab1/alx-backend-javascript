6c10e6078590961a7df9b25c6f0ace8b80ac8fdc
AhmedShehab1 <legendbtw2003@gmail.com>
2024-10-04 23:11:29 -0700

    Fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	let p1 = signUpUser(firstName, lastName);
-	let p2 = uploadPhoto(fileName);
+  const p1 = signUpUser(firstName, lastName);
+  const p2 = uploadPhoto(fileName);
 
-	Promise.allSettled([p1, p2]).then((results) => {
-		let results.forEach((result) => )
-	})
+  return Promise.allSettled([p1, p2]).then((results) => {
+    results.forEach((result) => ({
+      status: result.status,
+      value: result.value || result.reason,
+    }));
+  });
 }
