afb96033ff77d26cee9293f9c2d536f22fc75aaa
AhmedShehab1 <legendbtw2003@gmail.com>
2024-10-05 00:05:32 -0700

    Added Advanced Task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const p1 = await uploadPhoto();
+    const p2 = await createUser();
+
+    return {
+      photo: p1,
+      user: p2,
+    };
+  } catch (e) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
