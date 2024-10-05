2e9f48c5f59856dba6a3339cef5ae34700cbd690
AhmedShehab1 <legendbtw2003@gmail.com>
2024-10-04 22:45:50 -0700

    .

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   const p1 = uploadPhoto();
   const p2 = createUser();
 
-  Promise.all([p1, p2]).then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
+  return Promise.all([p1, p2]).then((values) => { console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
 }
