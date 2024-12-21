7a2fa528a885ccc9fcbf6a8aa80a2bbd7f665e54
Ahmed Shehab <legendbtw2003@gmail.com>
2024-12-21 20:12:25 +0000

    Added 2nd Test file

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -3,14 +3,11 @@ const calculateNumber = (type, a, b) => {
   switch (type) {
     case 'SUM':
       return Math.round(a) + Math.round(b);
-      break;
     case 'SUBTRACT':
-      return Math.round(b) - Math.round(a);
-      break;
+      return Math.round(a) - Math.round(b);
     case 'DIVIDE':
       if (Math.round(b) === 0) return 'Error';
       return Math.round(a) / Math.round(b);
-      break;
   }
 };
 module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+    it('should return 4', () => {
+        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    });
+    it('should return -2', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+    });
+    it('should return 0.25', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1, 4), 0.25);
+    });
+    it('should return 0.2', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1, 5), 0.2);
+    });
+    it('should return Error', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+    });
+});
